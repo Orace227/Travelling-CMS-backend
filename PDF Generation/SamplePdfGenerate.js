5d19a92059acaa27f8bb08cf0a54460364c8741d
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-09-03 12:01:09 +0530

    pdf formate is done

--- PDF Generation/SamplePdfGenerate.js
+++ PDF Generation/SamplePdfGenerate.js
@@ -21,56 +21,60 @@ export const SamplePdfGenerator = async (req, res) => {
     doc.pipe(res);
 
     // Add content to the PDF
-    doc.fontSize(20).text(`${packageData.packageName} 
-    `);
-    doc.fontSize(16).text(`Tour Details:`);
+    doc.fontSize(30).text(`${packageData.packageName}`);
+    doc.fontSize(20).text(`  `);
+    doc.fontSize(20).text(`    Tour Details:`);
     for (let i = 0; i < packageData.packageBody.tourDetails.length; i++) {
-      doc.fontSize(14).text(`
-    ${packageData.packageBody.tourDetails[i].title}:
-    
-    ${packageData.packageBody.tourDetails[i].description}
+      doc.fontSize(10).text(`  `);
+      doc.fontSize(14)
+        .text(`            ${packageData.packageBody.tourDetails[i].title}:
+    `);
+      doc
+        .fontSize(14)
+        .text(`            ${packageData.packageBody.tourDetails[i].description}`);
+    }
+    doc.fontSize(20).text(`  `);
+    //fechting out the inclusion and exclusion//
+    doc.fontSize(20).text(`    Inclusions And Exclusions:`);
+    doc.fontSize(18).text(`  `);
+    doc.fontSize(18).text(`          Inclusions:`);
+    doc.fontSize(10).text(`  `);
+    for (let i = 0; i < packageData.packageBody.inclusionsAndExclusions.exclusions.length; i++){
+
+    doc.fontSize(14)
+        .text(`                   * ${packageData.packageBody.inclusionsAndExclusions.inclusions[i]}
     `);
     }
-    doc.fontSize(12).text(`
-    `);
-    doc.fontSize(18).text(`Inclusions And Exclusions`);
-    doc.fontSize(16).text(`
-    Inclusions:`);
+    doc.fontSize(18).text(`          Exclusions:`);
 
-    doc.fontSize(14).text(`
-      ${packageData.packageBody.inclusionsAndExclusions.inclusions}
-    `);
-    doc.fontSize(16).text(`
-    Exclusions:`);
+    for (let i = 0; i < packageData.packageBody.inclusionsAndExclusions.exclusions.length; i++){
+    doc.fontSize(10).text(`  `);
+    doc
+      .fontSize(14)
+      .text(`                     * ${packageData.packageBody.inclusionsAndExclusions.exclusions[i]}`);
 
-    doc.fontSize(14).text(`
-      ${packageData.packageBody.inclusionsAndExclusions.exclusions}
+    }
     
-    `);
-    doc.fontSize(18).text(`Terms And Conditions`);
-    doc.fontSize(16).text(`
-    terms:`);
-    for (
-      var i = 0;
-      i < packageData.packageBody.termsAndConditions.terms.length;
-      i++
-    ) {
-      doc.fontSize(14).text(`
-      ${packageData.packageBody.termsAndConditions.terms[i]
-    }.
+    //fechting out the terms and conditions//
+    doc.fontSize(20).text(`  `);
+    doc.fontSize(20).text(`    Terms And Conditions:`);
+    doc.fontSize(18).text(`  `);
+    doc.fontSize(18).text(`          Terms:`);
+    doc.fontSize(10).text(`  `);
+    for (let i = 0; i < packageData.packageBody.termsAndConditions.terms.length; i++){
+
+    doc.fontSize(14)
+        .text(`                   * ${packageData.packageBody.termsAndConditions.terms[i]}
     `);
     }
-    doc.fontSize(16).text(`
-    conditions:`);
-    for (
-      var i = 0;
-      i < packageData.packageBody.termsAndConditions.terms.length;
-      i++
-    ) {
-      doc.fontSize(14).text(`
-      ${packageData.packageBody.termsAndConditions.conditions[i]
-    }.
-      `);
+    doc.fontSize(18).text(`          Conditions:`);
+
+    for (let i = 0; i < packageData.packageBody.termsAndConditions.conditions.length; i++){
+    doc.fontSize(10).text(`  `);
+    doc
+      .fontSize(14)
+      .text(`                     * ${packageData.packageBody.termsAndConditions.conditions[i]}`);
+
     }
     // Add more content as needed
 
