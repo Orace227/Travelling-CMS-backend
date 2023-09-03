1c010d1255ca3c417f0a6bcf5a960f48ff2915fb
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-09-03 13:19:59 +0530

    some chages in getDraftPackages

--- package/GetDraftPackage.js
+++ package/GetDraftPackage.js
@@ -1,8 +1,8 @@
 import Package from "../schemas/Package.js";
 
-export const GetDraftPackage= async (req, res) => {
+export const GetDraftPackage = async (req, res) => {
   try {
-    const allPackages = await Package.find({ isLive: false }, "-packagePrice");
+    const allPackages = await Package.find({ isLive: false });
     res.status(200).json({ allPackages });
   } catch (err) {
     res.status(500).json({ error: err.message });
