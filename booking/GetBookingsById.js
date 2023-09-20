3e13f158e2b7dbfd2e3c87c29d11efcc56fb9a86
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-09-20 18:52:43 +0530

    added read booking section

--- booking/GetBookingsById.js
+++ booking/GetBookingsById.js
@@ -2,18 +2,18 @@ import Booking from "../schemas/Booking.js";
 
 export const GetBookingsById = async (req, res) => {
   try {
-    const { clientId } = req.query;
-    const existedBookings = await Booking.findOne({ clientId });
+    const { bookingId } = req.query;
+    const existedBookings = await Booking.findOne({ bookingId });
     // console.log(allBookings);
 
     if (existedBookings) {
-      const allBookings = await Booking.find({ clientId });
+      const allBookings = await Booking.find({ bookingId });
       if (allBookings.length > 0) {
         res.status(200).json({ allBookings });
       }
     } else {
       res.status(404).json({
-        error: `Any bookings are Not Found for client id ${clientId}`,
+        error: `Any bookings are Not Found for client id ${bookingId}`,
       });
     }
   } catch (err) {
