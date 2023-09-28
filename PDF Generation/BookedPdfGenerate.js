65eb785bf41859e38f829248aa2e2597869cbf67
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-09-28 18:36:10 +0530

    make pdf generation better

--- PDF Generation/BookedPdfGenerate.js
+++ PDF Generation/BookedPdfGenerate.js
@@ -1,256 +1,247 @@
 import Package from "../schemas/Package.js";
 import Client from "../schemas/Client.js";
 import Booking from "../schemas/Booking.js";
-import PDFdocument from "pdfkit";
+import PDFDocument from "pdfkit";
 import fs from "fs";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import jsdom from "jsdom";
+const { JSDOM } = jsdom;
 
 export const BookedPdfGenerator = async (req, res) => {
   try {
-    console.log(__dirname);
     const { clientId, packageId, bookingId } = req.query;
-    console.log({ clientId, packageId, bookingId });
 
-    // const imagePath = path.join(
-    //   "C:\\Users\\gklib\\OneDrive\\Documents\\Projects\\Travelling CMS\\backend\\Assests",
-    //   "banner.jpg"
-    // );
-
-    // * Fetch data from the database for package details
     const packageData = await Package.findOne({ PackageId: packageId });
-
-    // * Fetch data from the database for client details
     const client = await Client.findOne({ clientId });
-
-    // const familyData = await FamilyMember.find({
-    //   clientId,
-    // });
-    // * Fetch data from the database for booking details
     const booking = await Booking.findOne({ bookingId });
-
     console.log({
-      client,
       packageData,
+      client,
+      booking124: booking,
       booking: booking.bookingDetails,
-      // hotelDetails: booking.hotelDetails,
-      // flightDetails: booking.flightDetails,
     });
-
-    const basePath = path.join(__dirname, ".."); // Go up one directory from the current __dirname
-    // console.log(basePath);
-
+    const basePath = path.join(__dirname, "..");
     const imagePath = path.join(basePath, packageData.packageImgPath);
     const imageBuffer = fs.readFileSync(imagePath);
-    // Create a new PDF document using pdfkit
-    const doc = new PDFdocument();
 
-    // Set response headers for PDF
+    const doc = new PDFDocument({
+      size: "letter",
+      margin: 50,
+    });
+
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader(
       "Content-Disposition",
-      `inline; filename="${packageData.packageName}"`
+      `inline; filename="${packageData.packageName}.pdf"`
     );
 
-    // Pipe the PDF content to the response
+    // Set precise margins for the entire page
+    const margin = 30;
+    doc.page.margins = {
+      top: margin,
+      bottom: margin,
+      left: margin,
+      right: margin,
+    };
+
     doc.pipe(res);
-
-    //////////////////// * added package details in the pdf document //////////////////////////////////////////
-
-    // Add content to the PDF
-    doc.fontSize(30).text(`${packageData.packageName}`);
-    doc.fontSize(20).text(`  `);
-
-    doc.image(imageBuffer, {
-      fit: [470, 300], // Set the width and height of the image
+    // Add package name with formatting and justification
+    doc.fontSize(30).text(packageData.packageName, {
+      align: "center",
+      margin: { top: 5, bottom: 20 },
+      align: "justify",
     });
-
-    doc.fontSize(16).text(`  `);
-    doc.fontSize(20).text(`    Tour Details:`);
-    for (let i = 0; i < packageData.packageBody.tourDetails.length; i++) {
-      doc.fontSize(10).text(`  `);
-      doc
-        .fontSize(18)
-        .text(`          ${packageData.packageBody.tourDetails[i].title}:`);
-      doc.fontSize(10).text(`  `);
-      doc
-        .fontSize(14)
-        .text(
-          `                   * ${packageData.packageBody.tourDetails[i].description}`
-        );
-    }
+    // Set the image width to cover the full width of the PDF page
+    doc.image(imageBuffer, {
+      fit: [500, 300], // Use pdfPageWidth as the width here
+      align: "left",
+    });
     doc.fontSize(20).text(`  `);
-    //fechting out the inclusion and exclusion//
-    doc.fontSize(20).text(`    Inclusions And Exclusions:`);
-    doc.fontSize(10).text(`  `);
-    doc.fontSize(18).text(`          Inclusions:`);
-    doc.fontSize(10).text(`  `);
-    for (
-      let i = 0;
-      i < packageData.packageBody.inclusionsAndExclusions.inclusions.length;
-      i++
-    ) {
-      doc.fontSize(14)
-        .text(`                   * ${packageData.packageBody.inclusionsAndExclusions.inclusions[i]}
-    `);
-    }
-    doc.fontSize(18).text(`          Exclusions:`);
 
-    for (
-      let i = 0;
-      i < packageData.packageBody.inclusionsAndExclusions.exclusions.length;
-      i++
-    ) {
-      doc.fontSize(10).text(`  `);
-      doc
-        .fontSize(14)
-        .text(
-          `                     * ${packageData.packageBody.inclusionsAndExclusions.exclusions[i]}`
-        );
-    }
+    // Add tour details
+    await addHtmlSection(
+      doc,
+      "Tour Details",
+      packageData.packageBody.tourDetails
+    );
 
-    //fechting out the terms and conditions//
     doc.fontSize(20).text(`  `);
-    doc.fontSize(20).text(`    Terms And Conditions:`);
-    doc.fontSize(10).text(`  `);
-    const titleX = 120; // Customize the X-coordinate as needed
-    doc.fontSize(18).text("Terms:", titleX, doc.y);
-    doc.fontSize(10).text(`  `);
-    for (
-      let i = 0;
-      i < packageData.packageBody.termsAndConditions.terms.length;
-      i++
-    ) {
-      doc
-        .fontSize(14)
-        .text(
-          `         ${i + 1}. ${
-            packageData.packageBody.termsAndConditions.terms[i]
-          }`,
-          {
-            align: "justify",
-          }
-        );
-      doc.fontSize(10).text(`  `);
-    }
-    doc.fontSize(18).text(`          Conditions:`);
 
-    for (
-      let i = 0;
-      i < packageData.packageBody.termsAndConditions.conditions.length;
-      i++
-    ) {
-      doc.fontSize(10).text(`  `);
-      doc
-        .fontSize(14)
-        .text(
-          `                     * ${packageData.packageBody.termsAndConditions.conditions[i]}`
-        );
-    }
+    // Add inclusions and exclusions
+    addSection(
+      doc,
+      "Inclusions",
+      packageData.packageBody.inclusionsAndExclusions.inclusions
+    );
 
-    // * fechting out the price//
     doc.fontSize(20).text(`  `);
-    doc.fontSize(20).text(`    Package Price: ${booking.modifiedPackagePrice}`);
 
-    //////////////////// * ADDED client details in the pdf document //////////////////////////////////////////
+    addSection(
+      doc,
+      "Exclusions",
+      packageData.packageBody.inclusionsAndExclusions.exclusions
+    );
+
     doc.fontSize(20).text(`  `);
-    doc.fontSize(20).text(`    Client Details:`);
-    doc.fontSize(10).text(`  `);
+
+    // Add terms and conditions
+    addSection(doc, "Terms", packageData.packageBody.termsAndConditions.terms);
+
+    doc.fontSize(20).text(`  `);
+
+    addSection(
+      doc,
+      "Conditions",
+      packageData.packageBody.termsAndConditions.conditions
+    );
+
+    doc.fontSize(20).text(`  `);
+
+    // Add package price if it's not live
+    if (!packageData.isLive) {
+      doc.fontSize(18).text(`Package Price: ${booking.modifiedPackagePrice}`, {
+        align: "center",
+        margin: { top: 20, bottom: 20 },
+        align: "justify",
+      });
+    }
+
+    doc.fontSize(20).text(`   `);
+    doc.fontSize(20).text(`Client Details:`);
+    doc.fontSize(10).text(`   `);
     doc
       .fontSize(16)
-      .text(`          Name: ${client.firstName} ${client.lastName}`);
+      .text(`    Client Name: ${client.firstName} ${client.lastName}`, {
+        margin: { top: 5, bottom: 20 },
+        align: "justify",
+      });
 
-    // //////////////////// * ADDED Booking details in the pdf document //////////////////////////////////////////
-
-    doc.fontSize(20).text(`  `);
-    doc.fontSize(20).text(`     Booking Details:`);
-    doc.fontSize(10).text(`  `);
+    doc.fontSize(20).text(`   `);
+    doc.fontSize(20).text(`Booking Details:`);
+    doc.fontSize(10).text(`   `);
 
     for (let i = 0; i < booking.bookingDetails.length; i++) {
       doc
-        .fontSize(18)
-        .text(`          ${booking.bookingDetails[i].bookingType} Details:`);
-      doc.fontSize(10).text(`  `);
-      doc
         .fontSize(16)
         .text(
-          `            ${i + 1}.  ${
-            booking.bookingDetails[i].bookingType
-          } Name: ${booking.bookingDetails[0].bookingName}`
+          `    ${i + 1}. booking Name: ${
+            booking.bookingDetails[i].bookingName
+          }`,
+          {
+            align: "left",
+            margin: { top: 5, bottom: 20 },
+            align: "justify",
+          }
         );
-      doc.fontSize(12).text(`  `);
-      let link = `http://localhost:7000/${booking.bookingDetails[i].docImgPath}`;
-      console.log(`document link(${i}):`, link);
+      doc.fontSize(10).text(`   `);
+
+      let link = `${process.env.link}${booking.bookingDetails[i].docImgPath}`;
+      // console.log(`document link(${i}):`, link);
+
       const buttonText = `Open Link`;
-      const text = `                     * Document for ${booking.bookingDetails[i].bookingName}: ${buttonText}`;
+      const text = `               * Document for ${booking.bookingDetails[i].bookingName}: ${buttonText}`;
       doc.fontSize(14).text(text, { link: link, continued: false });
       doc.fontSize(12).text(`  `);
     }
-
-    //   doc
-    //     .fontSize(14)
-    //     .text(
-    //       `                      * Check Out Date: ${booking.hotelDetails[i].checkOutDate}`
-    //     );
-    //   doc.fontSize(12).text(`  `);
-    //   doc
-    //     .fontSize(14)
-    //     .text(`                      * Document: <LINK OF THE FILE>`);
-    //   doc.fontSize(12).text(`  `);
-    // }
-
-    // doc.fontSize(20).text(`  `);
-    // doc.fontSize(18).text(`          Flight Details:`);
-    // doc.fontSize(18).text(`  `);
-
-    // for (let i = 0; i < booking.flightDetails.length; i++) {
-    //   doc
-    //     .fontSize(16)
-    //     .text(
-    //       `            ${i + 1}.  Airline Name: ${
-    //         booking.flightDetails[i].airline
-    //       }`
-    //     );
-    //   doc.fontSize(12).text(`  `);
-
-    //   doc
-    //     .fontSize(14)
-    //     .text(
-    //       `                      * Departure Date: ${booking.flightDetails[i].departureDate}`
-    //     );
-    //   doc.fontSize(12).text(`  `);
-    //   doc
-    //     .fontSize(14)
-    //     .text(
-    //       `                      * Return Date: ${booking.flightDetails[i].returnDate}`
-    //     );
-    //   doc.fontSize(12).text(`  `);
-    //   doc
-    //     .fontSize(14)
-    //     .text(`                      * Document: <LINK OF THE FILE>`);
-    //   doc.fontSize(12).text(`  `);
-    // }
-    // doc.fontSize(10).text(`  `);
-    // doc.fontSize(18).text(`          Receipt Details:`);
-    // doc.fontSize(10).text(`  `);
-
-    // doc
-    //   .fontSize(16)
-    //   .text(
-    //     `              Payment Method: ${booking.receiptDetails.paymentMethod}`
-    //   );
-    // doc.fontSize(12).text(`  `);
-
-    // Add more content as needed
-
-    // End the PDF document
     doc.end();
-
-    // console.log(packageData);
   } catch (error) {
     console.error("Error generating PDF:", error);
     res.status(500).json({ error: "Error generating PDF" });
   }
 };
+
+// Function to add a section with title and items
+function addSection(doc, title, items) {
+  if (items && items.length > 0) {
+    // Add section title with formatting and justification
+    doc.fontSize(26).text(title, {
+      margin: { top: 30, bottom: 20 },
+      align: "justify",
+    });
+    doc.fontSize(10).text(`  `);
+
+    items.forEach((item) => {
+      // Add each item with bullet points, spacing, and justification
+      doc.fontSize(14).list([`- ${item}`], {
+        bulletRadius: 3,
+        bulletIndent: 10,
+        width: 500,
+        align: "justify",
+      });
+      doc.fontSize(10).text(`  `);
+    });
+  }
+}
+
+//// Function to add a section with title and HTML content
+async function addHtmlSection(doc, title, htmlContent) {
+  if (htmlContent) {
+    // Add section title with formatting and justification
+    doc.fontSize(26).text(title, {
+      margin: { top: 30, bottom: 20 },
+      align: "justify",
+    });
+    doc.fontSize(10).text(`  `);
+
+    // Parse HTML content using jsdom
+    const dom = new JSDOM(htmlContent);
+    const body = dom.window.document.body;
+
+    // Function to handle different HTML elements
+    const processNode = (node) => {
+      // console.log(node.nodeName);
+      switch (node.nodeName) {
+        case "H2":
+          // Handle <h1> as a header
+          doc.fontSize(20).text(node.textContent, {
+            bold: true,
+            margin: { top: 10 },
+            align: "justify",
+          });
+          break;
+        case "P":
+          // Handle <p> as a paragraph
+          doc.fontSize(14).text(node.textContent, {
+            margin: { top: 5 },
+            align: "justify",
+          });
+          break;
+
+        case "BR":
+          doc.fontSize(10).text(`  `);
+          break;
+        case "UL":
+          // Handle <ul> as a bullet list
+          const items = node.querySelectorAll("li");
+          items.forEach((item) => {
+            doc.fontSize(14).list([item.textContent], {
+              bulletRadius: 3,
+              bulletIndent: 10,
+              width: 480,
+              align: "justify",
+            });
+          });
+          break;
+        // Add more cases for other HTML elements as needed
+        default:
+          // Handle other elements as plain text
+          // doc.fontSize(14).text(node.textContent, {
+          //   margin: { top: 5 },
+          //   align: "justify",
+          // });
+          break;
+      }
+
+      // Recursively process child nodes
+      node.childNodes.forEach(processNode);
+    };
+
+    // Start processing from the root of the document body
+    processNode(body);
+
+    doc.fontSize(10).text(`  `);
+  }
+}
--- PDF Generation/SamplePdfGenerate.js
+++ PDF Generation/SamplePdfGenerate.js
@@ -1,113 +1,199 @@
 import Package from "../schemas/Package.js";
 import PDFDocument from "pdfkit";
+import fs from "fs";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import jsdom from "jsdom";
+const { JSDOM } = jsdom;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const SamplePdfGenerator = async (req, res) => {
   try {
     const packageId = req.params.id;
-    // console.log(packageId);
-    // Fetch data from the database using Mongoose
     const packageData = await Package.findOne({ PackageId: packageId });
 
-    // Create a new PDF document using pdfkit
+    const basePath = path.join(__dirname, "..");
+    const imagePath = path.join(basePath, packageData.packageImgPath);
+    const imageBuffer = fs.readFileSync(imagePath);
+
+    // Calculate the width of the PDF page
     const doc = new PDFDocument();
-    // Set response headers for PDF
+
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader(
       "Content-Disposition",
       `inline; filename="${packageData.packageName}.pdf"`
     );
 
-    // Pipe the PDF content to the response
+    // Set precise margins for the entire page
+    const margin = 30;
+    doc.page.margins = {
+      top: margin,
+      bottom: margin,
+      left: margin,
+      right: margin,
+    };
+
     doc.pipe(res);
-
-    // Add content to the PDF
-    doc.fontSize(30).text(`${packageData.packageName}`);
+    // Add package name with formatting and justification
+    doc.fontSize(30).text(packageData.packageName, {
+      align: "center",
+      margin: { top: 5, bottom: 20 },
+      align: "justify",
+    });
+    // Set the image width to cover the full width of the PDF page
+    doc.image(imageBuffer, {
+      fit: [490, 300], // Use pdfPageWidth as the width here
+      align: "left",
+    });
     doc.fontSize(20).text(`  `);
-    doc.fontSize(20).text(`    Tour Details:`);
-    for (let i = 0; i < packageData.packageBody.tourDetails.length; i++) {
-      doc.fontSize(10).text(`  `);
-      doc.fontSize(14)
-        .text(`            ${packageData.packageBody.tourDetails[i].title}:
-    `);
-      doc
-        .fontSize(14)
-        .text(
-          `            ${packageData.packageBody.tourDetails[i].description}`
-        );
-    }
+
+    // Add tour details
+    await addHtmlSection(
+      doc,
+      "Tour Details",
+      packageData.packageBody.tourDetails
+    );
+
     doc.fontSize(20).text(`  `);
-    //fechting out the inclusion and exclusion//
-    doc.fontSize(20).text(`    Inclusions And Exclusions:`);
-    doc.fontSize(18).text(`  `);
-    doc.fontSize(18).text(`          Inclusions:`);
-    doc.fontSize(10).text(`  `);
-    for (
-      let i = 0;
-      i < packageData.packageBody.inclusionsAndExclusions.exclusions.length;
-      i++
-    ) {
-      doc.fontSize(14)
-        .text(`                   * ${packageData.packageBody.inclusionsAndExclusions.inclusions[i]}
-    `);
-    }
-    doc.fontSize(18).text(`          Exclusions:`);
 
-    for (
-      let i = 0;
-      i < packageData.packageBody.inclusionsAndExclusions.exclusions.length;
-      i++
-    ) {
-      doc.fontSize(10).text(`  `);
-      doc
-        .fontSize(14)
-        .text(
-          `                     * ${packageData.packageBody.inclusionsAndExclusions.exclusions[i]}`
-        );
-    }
+    // Add inclusions and exclusions
+    addSection(
+      doc,
+      "Inclusions",
+      packageData.packageBody.inclusionsAndExclusions.inclusions
+    );
 
-    //fechting out the terms and conditions//
     doc.fontSize(20).text(`  `);
-    doc.fontSize(20).text(`    Terms And Conditions:`);
-    doc.fontSize(18).text(`  `);
-    doc.fontSize(18).text(`          Terms:`);
-    doc.fontSize(10).text(`  `);
-    for (
-      let i = 0;
-      i < packageData.packageBody.termsAndConditions.terms.length;
-      i++
-    ) {
-      doc.fontSize(14)
-        .text(`                   * ${packageData.packageBody.termsAndConditions.terms[i]}
-    `);
-    }
-    doc.fontSize(18).text(`          Conditions:`);
 
-    for (
-      let i = 0;
-      i < packageData.packageBody.termsAndConditions.conditions.length;
-      i++
-    ) {
-      doc.fontSize(10).text(`  `);
-      doc
-        .fontSize(14)
-        .text(
-          `                     * ${packageData.packageBody.termsAndConditions.conditions[i]}`
-        );
-    }
+    addSection(
+      doc,
+      "Exclusions",
+      packageData.packageBody.inclusionsAndExclusions.exclusions
+    );
 
-    //fechting out the price//
-    if (packageData.isLive == false) {
-      doc.fontSize(30).text(`  `);
-      doc.fontSize(20).text(`     Package Price: ${packageData.packagePrice}`);
+    doc.fontSize(20).text(`  `);
+
+    // Add terms and conditions
+    addSection(doc, "Terms", packageData.packageBody.termsAndConditions.terms);
+
+    doc.fontSize(20).text(`  `);
+
+    addSection(
+      doc,
+      "Conditions",
+      packageData.packageBody.termsAndConditions.conditions
+    );
+
+    doc.fontSize(20).text(`  `);
+
+    // Add package price if it's not live
+    if (!packageData.isLive) {
+      doc.fontSize(20).text(`Package Price: ${packageData.packagePrice}`, {
+        align: "center",
+        margin: { top: 20, bottom: 20 },
+        align: "justify",
+      });
     }
 
-    // Add more content as needed
-
-    // End the PDF document
     doc.end();
-    // console.log(packageData);
   } catch (error) {
     console.error("Error generating PDF:", error);
     res.status(500).json({ error: "Error generating PDF" });
   }
 };
+
+// Function to add a section with title and items
+function addSection(doc, title, items) {
+  if (items && items.length > 0) {
+    // Add section title with formatting and justification
+    doc.fontSize(26).text(title, {
+      margin: { top: 30, bottom: 20 },
+      align: "justify",
+    });
+    doc.fontSize(10).text(`  `);
+
+    items.forEach((item) => {
+      // Add each item with bullet points, spacing, and justification
+      doc.fontSize(14).list([`- ${item}`], {
+        bulletRadius: 3,
+        bulletIndent: 10,
+        width: 480,
+        align: "justify",
+      });
+      doc.fontSize(10).text(`  `);
+    });
+  }
+}
+
+//// Function to add a section with title and HTML content
+async function addHtmlSection(doc, title, htmlContent) {
+  if (htmlContent) {
+    // Add section title with formatting and justification
+    doc.fontSize(26).text(title, {
+      margin: { top: 30, bottom: 20 },
+      align: "justify",
+    });
+    doc.fontSize(10).text(`  `);
+
+    // Parse HTML content using jsdom
+    const dom = new JSDOM(htmlContent);
+    const body = dom.window.document.body;
+
+    // Function to handle different HTML elements
+    const processNode = (node) => {
+      console.log(node.nodeName);
+      switch (node.nodeName) {
+        case "H2":
+          // Handle <h1> as a header
+          doc.fontSize(20).text(node.textContent, {
+            bold: true,
+            margin: { top: 10 },
+            align: "justify",
+          });
+          break;
+        case "P":
+          // Handle <p> as a paragraph
+          doc.fontSize(14).text(node.textContent, {
+            margin: { top: 5 },
+            align: "justify",
+          });
+          break;
+
+        case "BR":
+          doc.fontSize(10).text(`  `);
+          break;
+        case "UL":
+          // Handle <ul> as a bullet list
+          const items = node.querySelectorAll("li");
+          items.forEach((item) => {
+            doc.fontSize(14).list([item.textContent], {
+              bulletRadius: 3,
+              bulletIndent: 10,
+              width: 480,
+              align: "justify",
+            });
+          });
+          break;
+        // Add more cases for other HTML elements as needed
+        default:
+          // Handle other elements as plain text
+          // doc.fontSize(14).text(node.textContent, {
+          //   margin: { top: 5 },
+          //   align: "justify",
+          // });
+          break;
+      }
+
+      // Recursively process child nodes
+      node.childNodes.forEach(processNode);
+    };
+
+    // Start processing from the root of the document body
+    processNode(body);
+
+    doc.fontSize(10).text(`  `);
+  }
+}
