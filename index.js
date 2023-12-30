400765dffe2cbb6ffc9df2991e0f28385d9ba451
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-12-30 15:41:29 +0530

    changed link of pdf

--- index.js
+++ index.js
@@ -57,11 +57,7 @@ const db = connectToMongo();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      process.env.CLIENT_URL_1,
-      process.env.CLIENT_URL_2,
-      process.env.CLIENT_URL_3,
-    ],
+    origin: [process.env.CLIENT_URL_1, process.env.CLIENT_URL_2],
     // origin: "http://localhost:3000",
     methods: ["GET", "POST", "UPDATE", "DELETE"],
     credentials: true,
